4de71b4d1818debea6380d22d8912cd935e6cde7
Mehmet Duran <mehmet.duran@gmail.com>
2015-06-25 12:13:46 +0100

    Minor changes after user feedback

--- js/prototype.js
+++ js/prototype.js
@@ -5,12 +5,29 @@ $(function (){
     event.preventDefault();
 
     if( !$selectAll.hasClass('select-none') ){
-      console.log($('.claim-list-table input'));
       $('.claim-list-table input').prop('checked', true);
       $selectAll.addClass('select-none').html('Select none');
     }else{
       $('.claim-list-table input').prop('checked', false);
       $selectAll.removeClass('select-none').html('Select all');
+    }
+  });
+
+  $('.allocate-button').click(function ( event ){
+    $('.allocation-summary').removeClass('hidden');
+  });
+
+  var $highValue = $('.high-value-only');
+
+  $highValue.click(function ( event ){
+    event.preventDefault();
+
+    if( !$highValue.hasClass('selected') ){
+      $highValue.addClass('selected');
+      $highValue.html('Show claims for all values');
+    }else{
+      $highValue.removeClass('selected');
+      $highValue.html('Show only high value claims');
     }
   });
 
